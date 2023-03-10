304ec73f8065f038332df3f88bca6233ee6bab40
Nishant Racherla <nishant.racherla@gmail.com>
2023-03-10 20:05:54 +0530

    added apollo client

--- client/index.js
+++ client/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-const Root = () => <div>Hello World!</div>;
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id || null
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  }
+});
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Root />);
